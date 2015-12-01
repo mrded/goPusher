2578b1dcbc01ea5e786cff422c50bffa35f0cbbf
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-12-01 13:49:34 +0000

    Delete saving log into file. Let supervisor to do that.

--- src/github.com/mrded/goPusher/cfg/cfg.go
+++ src/github.com/mrded/goPusher/cfg/cfg.go
@@ -6,7 +6,6 @@ import log "gopkg.in/inconshreveable/log15.v2"
 type Options struct {
   Port    string
   Token   string
-  Logs    string
 }
 
 var options Options
@@ -24,8 +23,4 @@ func init() {
 
   options.Port = cfg.Section("").Key("port").String()
   options.Token = cfg.Section("").Key("token").String()
-  options.Logs = cfg.Section("").Key("logs").String()
-  
-  logHandler := log.MultiHandler(log.StdoutHandler, log.Must.FileHandler(options.Logs + "/stdout.log", log.LogfmtFormat()))
-  log.Root().SetHandler(logHandler)
 }
